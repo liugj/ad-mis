cd9e731c469032d948fe7b55499391cabfcefde8
work <liugj@126.com>
2015-08-26 13:46:57 +0800

    增加cmd命令

--- app/Console/Commands/Index.php
+++ app/Console/Commands/Index.php
@@ -43,6 +43,8 @@ class Index extends Command implements SelfHandling
         
         if ('prod' == env('APP_ENV')){
             $cmd = sprintf('%s  --source=mysql://%s:%s@127.0.0.1:%d/mis --sql="select dm.id as id , name,comment,device_id,media_id,classify_id, classify_id_1,classify_id_3,group_id from medias  as m join device_media  as dm on dm.media_id=m.id" media  --rebuild',self :: INDEXCMD, env('DB_USERNAME'), env('DB_PASSWORD'), env('DB_PORT'));
+            system($cmd);
+            log :: info (env('APP_ENV'). ' '. $cmd);
             }else{
             log :: info (env('APP_ENV'). 'not run.');
         }
