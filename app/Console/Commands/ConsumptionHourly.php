39a7363b1a0aca5227f4154ac3e4c326f7a5d878
work <liugj@126.com>
2015-08-27 11:42:51 +0800

    保留dpid为空的情况

--- app/Console/Commands/ConsumptionHourly.php
+++ app/Console/Commands/ConsumptionHourly.php
@@ -217,8 +217,9 @@ class ConsumptionHourly extends Command implements SelfHandling
         }
         foreach ($stats as &$stat) {
             foreach (self ::$statsCountUniqItems as $key=>$value) {
-                if (isset($stat[$value]))  $stat[$value] = count($stat[$value]);
-                else $stat[$value] = 0;
+                if (isset($stat[$value])){
+                      $stat[$value] = count($stat[$value]);
+                }else $stat[$value] = 0;
             }
         }
         return $stats;
